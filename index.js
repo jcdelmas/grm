df1521df772e6ce3da70322a7cbe329ab4116414
Jean-Christophe Delmas <jc.delmas@zippyguide.com>
2015-10-06 11:47:12 +0200

    chore: improve build process

--- index.js
+++
@@ -1,8 +0,0 @@
-
-import grm from './lib/grm';
-import ast from './lib/ast';
-import { Relations } from './lib/constants';
-
-export const sql = ast;
-export { Relations };
-export default grm;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import grm from './grm';
+import ast from './ast';
+import { Relations } from './constants';
+
+export const sql = ast;
+export { Relations };
+export default grm;
